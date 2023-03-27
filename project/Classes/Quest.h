a529e744335abafe50e1ce1440a1101730d96ee5
Michal Walach <michal.walach111@gmail.com>
2023-03-27 19:55:45 +0200

    added Quest display function

--- project/Classes/Quest.h
+++ project/Classes/Quest.h
@@ -6,9 +6,14 @@ class Quest{
     unsigned int demanded_quantity, deadline;
 public:
     Quest(string, string, int, int);
+    void DisplayQuest();
 };
 
 Quest::Quest(string qname, string ditem, int dquantity, int _deadline):quest_name(qname),demanded_item(ditem){
     demanded_quantity = (rand()%(dquantity/5) - dquantity/10) + dquantity;
     deadline = (rand()%(_deadline/5) - _deadline/10) + _deadline;
+}
+
+void Quest::DisplayQuest(){
+    cout << quest_name << ": get " << demanded_quantity << " " << demanded_item << "s in " << deadline << " cycles" << endl;
 }
