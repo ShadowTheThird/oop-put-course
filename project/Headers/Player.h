6d58d87d65fd9b6a69ec0d2bb7ec7704fbc658be
ShadowTheThird <michal.walach111@gmail.com>
2023-05-26 19:17:22 +0200

    changed player class that uses entity

--- project/Headers/Player.h
+++ project/Headers/Player.h
@@ -1,17 +1,18 @@
 #include <string>
+#include "Entity.h"
+#include "Moves.h"
 using namespace std;
 
-class Player{
+class Player: protected Entity{
 protected:
-    string name;
-    int hp_player, dmg_player;
+    
 public:
-    Player(string _name, int difficulty):name(_name), hp_player(difficulty==1?100:difficulty==2?75:difficulty==3?50:0), dmg_player(difficulty==1?20:difficulty==2?15:difficulty==3?10:0){}
+    Player(string _name, int difficulty):Entity(difficulty==1?100:difficulty==2?75:difficulty==3?50:0, difficulty==1?20:difficulty==2?15:difficulty==3?10:0, _name, 4){}
     bool IsDead();
 };
 
 bool Player::IsDead(){
-    if(hp_player <= 0){
+    if(hp <= 0){
         return true;
     }
     return false;
