c9589e116a8632362796825383f9ae7c12cd1925
ShadowTheThird <michal.walach111@gmail.com>
2023-05-27 00:11:57 +0200

    implemented skirmish system

--- project/Headers/Command.h
+++ project/Headers/Command.h
@@ -1,15 +1,15 @@
 #include <iostream>
-#include "Enemies.h"
+#include "SkirmishEntity.h"
 using namespace std;
 
 class Command{
 public:
     static void Exit();
-    static Entity Fight(Player, Enemies);
+    static Enemy Fight(Player, Enemies);
     static void Help();
     static void Inspect(Player);
     static void Skills(Player, Moves);
-    static bool Skirmish(Player, Entity);
+    static bool Skirmish(Player, Enemy, Moves);
 };
 
 void Command::Exit(){
@@ -17,7 +17,7 @@ void Command::Exit(){
     exit(0);
 }
 
-Entity Command::Fight(Player player, Enemies database){
+Enemy Command::Fight(Player player, Enemies database){
     return database.SelectEnemy(rand()%8, player.EnemyLevel());
 }
 
@@ -33,11 +33,27 @@ void Command::Skills(Player player, Moves temp){
     player.PlayerSkills(temp);
 }
 
-bool Command::Skirmish(Player player, Entity enemy){
+bool Command::Skirmish(Player player, Enemy enemy, Moves abilities){
     cout << "\tYou are facing " << enemy.Name() << endl;
-    // while(enemy.Alive()){
-    //     cout << "\t" << enemy.Name() << " has " << enemy.Health() << "hp. you can use:" << endl;
-
-    // }
+    short id;
+    while(enemy.Alive()){
+        SkirmishEntity temp(enemy, player);
+        cout << "\t" << enemy.Name() << " has " << temp.EnemyHealth() << "hp. you have " << temp.PlayerHealth() << "hp. You can use:" << endl;
+        Skills(player, abilities);
+        cout << "\tChoose your move" << endl;
+        cin >> id;
+        if(!player.KnowsSkill(--id)){
+            cout << "\tException: skill identifier out of bounds" << endl;
+            continue;
+        }
+        temp.PlayerAttack(id, abilities);
+        temp.EnemyAttack(abilities);
+        player = temp.CreatePlayer();
+        enemy = temp.CreateEnemy();
+        if(!player.Alive()){
+            return false;
+        }
+    }
+    cout << "\t" << enemy.Name() << " has been slain" << endl;
     return true;
 }
