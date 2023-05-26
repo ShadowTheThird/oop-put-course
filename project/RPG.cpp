16b2de6a9e36c23b718efff5c9d3198996f559c1
ShadowTheThird <michal.walach111@gmail.com>
2023-05-26 18:23:40 +0200

    working player creator

---
+++ project/RPG.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "Headers\Player.h"
+#include "Headers\Enemy.h"
+using namespace std;
+
+string Lower(string text){
+    string lower_text = "";
+    for(int i = 0; text[i] != '\0'; i++){
+        lower_text += tolower(text[i]);
+    }
+    return lower_text;
+}
+
+int main(){
+    string command;
+    cout << "\tWelcome to rpg minigame!" << endl << "\tInput your player name" << endl;
+    getline(cin, command);
+    cout << "\tInput preffered difficulty:\n\t1. Easy\n\t2. Normal\n\t3. Hard" << endl;
+    int difficulty;
+    cin >> difficulty;
+    Player player(command, difficulty);
+    if(!player.IsDead()){
+        cout << "\tAll good so far";
+    }
+}
