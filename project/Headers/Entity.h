d9bdc9c8baee144076654a3028b060f20acc3b54
ShadowTheThird <michal.walach111@gmail.com>
2023-05-27 00:11:11 +0200

    deleted Health() method

--- project/Headers/Entity.h
+++ project/Headers/Entity.h
@@ -9,7 +9,6 @@ protected:
 public:
     Entity(int _hp, int _dmg, string _name, int _moves_count):hp(_hp), dmg(_dmg), name(_name), moves_count(0), max_moves(_moves_count), available_moves(new short[_moves_count]){}
     bool Alive();
-    int Health();
     void LearnMove(short);
     string Name();
     Entity operator*(int);
@@ -20,10 +19,6 @@ bool Entity::Alive(){
         return true;
     }
     return false;
-}
-
-int Entity::Health(){
-    return hp;
 }
 
 void Entity::LearnMove(short id){
