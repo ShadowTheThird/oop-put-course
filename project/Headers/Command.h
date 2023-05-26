73d4d4b970fb9ee551e9be853f3a4df9cf434ef0
ShadowTheThird <michal.walach111@gmail.com>
2023-05-26 22:25:26 +0200

    added more commands

--- project/Headers/Command.h
+++ project/Headers/Command.h
@@ -1,12 +1,15 @@
 #include <iostream>
-#include "Player.h"
+#include "Enemies.h"
 using namespace std;
 
 class Command{
 public:
     static void Exit();
+    static Entity Fight(Player, Enemies);
     static void Help();
+    static void Inspect(Player);
     static void Skills(Player, Moves);
+    static bool Skirmish(Player, Entity);
 };
 
 void Command::Exit(){
@@ -14,10 +17,27 @@ void Command::Exit(){
     exit(0);
 }
 
+Entity Command::Fight(Player player, Enemies database){
+    return database.SelectEnemy(rand()%8, player.EnemyLevel());
+}
+
 void Command::Help(){
-    cout << "\thelp - shows this manual\n\tskills - show skills available to player\n\texit - exits the game" << endl;
+    cout << "\thelp - shows this manual\n\tfight - sends you into skirmish\n\tinspect - inspects your character\n\tskills - show skills available to player\n\texit - exits the game" << endl;
+}
+
+void Command::Inspect(Player player){
+    player.Inspect();
 }
 
 void Command::Skills(Player player, Moves temp){
     player.PlayerSkills(temp);
+}
+
+bool Command::Skirmish(Player player, Entity enemy){
+    cout << "\tYou are facing " << enemy.Name() << endl;
+    // while(enemy.Alive()){
+    //     cout << "\t" << enemy.Name() << " has " << enemy.Health() << "hp. you can use:" << endl;
+
+    // }
+    return true;
 }
