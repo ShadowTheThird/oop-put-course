45d7215f867cb25790ad8aa72e2ca2f2e11d0876
ShadowTheThird <michal.walach111@gmail.com>
2023-05-26 19:53:38 +0200

    Player class changed to utilize Moves class

--- project/Headers/Player.h
+++ project/Headers/Player.h
@@ -1,14 +1,12 @@
 #include <string>
-#include "Entity.h"
 #include "Moves.h"
 using namespace std;
 
 class Player: protected Entity{
-protected:
-    
 public:
-    Player(string _name, int difficulty):Entity(difficulty==1?100:difficulty==2?75:difficulty==3?50:0, difficulty==1?20:difficulty==2?15:difficulty==3?10:0, _name, 4){}
+    Player(string _name, int difficulty):Entity(difficulty==1?100:difficulty==2?75:difficulty==3?50:0, difficulty==1?20:difficulty==2?15:difficulty==3?10:0, _name, 4){available_moves[moves_count++] = 0; available_moves[moves_count++] = 4;}
     bool IsDead();
+    void PlayerSkills(Moves);
 };
 
 bool Player::IsDead(){
@@ -16,4 +14,10 @@ bool Player::IsDead(){
         return true;
     }
     return false;
+}
+
+void Player::PlayerSkills(Moves temp){
+    for(int i = 0; i < moves_count; i++){
+        cout << "\t" << i+1 << ". " << temp.SkillName(available_moves[i]) << endl;
+    }
 }
