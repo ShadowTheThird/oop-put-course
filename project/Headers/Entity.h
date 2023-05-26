ba187d701431a09caad11a9845900451f8633c37
ShadowTheThird <michal.walach111@gmail.com>
2023-05-26 22:23:14 +0200

    added methods to entity class

--- project/Headers/Entity.h
+++ project/Headers/Entity.h
@@ -5,7 +5,42 @@ class Entity{
 protected:
     int hp, dmg;
     string name;
-    short *available_moves, moves_count;
+    short *available_moves, moves_count, max_moves;
 public:
-    Entity(int _hp, int _dmg, string _name, int _moves_count):hp(_hp), dmg(_dmg), name(_name), moves_count(0), available_moves(new short[_moves_count]){}
+    Entity(int _hp, int _dmg, string _name, int _moves_count):hp(_hp), dmg(_dmg), name(_name), moves_count(0), max_moves(_moves_count), available_moves(new short[_moves_count]){}
+    bool Alive();
+    int Health();
+    void LearnMove(short);
+    string Name();
+    Entity operator*(int);
 };
+
+bool Entity::Alive(){
+    if(hp > 0){
+        return true;
+    }
+    return false;
+}
+
+int Entity::Health(){
+    return hp;
+}
+
+void Entity::LearnMove(short id){
+    if(moves_count == max_moves){
+        return;
+    }
+    available_moves[moves_count++] = id;
+}
+
+string Entity::Name(){
+    return name;
+}
+
+Entity Entity::operator*(int level){
+    Entity temp(*this);
+    temp.hp *= level;
+    temp.dmg *= level;
+    temp.name += " " + to_string(level);
+    return temp;
+}
