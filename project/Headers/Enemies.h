8d1c7033ae89c84f6b88e7611ec276b397ab6278
ShadowTheThird <michal.walach111@gmail.com>
2023-05-27 00:10:13 +0200

    created an Enemy class to help with skirmish system

--- project/Headers/Enemies.h
+++ project/Headers/Enemies.h
@@ -3,34 +3,48 @@
 #include "Player.h"
 using namespace std;
 
+class Enemy: public Entity{
+public:
+    Enemy(int _hp, int _dmg, string _name, int _moves_count):Entity(_hp, _dmg, _name, _moves_count){}
+    Enemy operator*(int);
+};
+
+Enemy Enemy::operator*(int level){
+    Enemy temp(*this);
+    temp.hp *= level;
+    temp.dmg *= level;
+    temp.name += " " + to_string(level);
+    return temp;
+}
+
 class Enemies{
-    vector<Entity> enemies;
+    vector<Enemy> enemies;
 public:
     Enemies();
-    Entity SelectEnemy(short, int);
+    Enemy SelectEnemy(short, int);
 };
 
 Enemies::Enemies(){
-    enemies.push_back(Entity(45, 25, "Goblin", 1));
+    enemies.push_back(Enemy(45, 25, "Goblin", 1));
     enemies[0].LearnMove(1);
-    enemies.push_back(Entity(40, 40, "Skeleton", 1));
-    enemies[1].LearnMove(3);
-    enemies.push_back(Entity(100, 20, "Paladin", 2));
+    enemies.push_back(Enemy(40, 30, "Skeleton", 1));
+    enemies[1].LearnMove(0);
+    enemies.push_back(Enemy(100, 20, "Paladin", 2));
     enemies[2].LearnMove(0);
     enemies[2].LearnMove(5);
-    enemies.push_back(Entity(60, 10, "Slime", 1));
+    enemies.push_back(Enemy(60, 10, "Slime", 1));
     enemies[3].LearnMove(2);
-    enemies.push_back(Entity(30, 60, "Drunkard", 1));
+    enemies.push_back(Enemy(30, 50, "Drunkard", 1));
     enemies[4].LearnMove(0);
-    enemies.push_back(Entity(50, 40, "Spirit", 2));
+    enemies.push_back(Enemy(50, 40, "Spirit", 2));
     enemies[5].LearnMove(2);
     enemies[5].LearnMove(4);
-    enemies.push_back(Entity(150, 20, "Orc", 1));
+    enemies.push_back(Enemy(100, 10, "Orc", 1));
     enemies[6].LearnMove(3);
-    enemies.push_back(Entity(120, 10, "Zombie", 1));
+    enemies.push_back(Enemy(120, 10, "Zombie", 1));
     enemies[7].LearnMove(0);
 }
 
-Entity Enemies::SelectEnemy(short id, int level){
+Enemy Enemies::SelectEnemy(short id, int level){
     return enemies[id] * level;
 }
