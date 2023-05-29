429664446a5de4cd4ab91c3940454bf0c0b8a6aa
ShadowTheThird <michal.walach111@gmail.com>
2023-05-29 20:12:14 +0200

    uabstrakcyjnienie

--- project/Headers/Entity.h
+++ project/Headers/Entity.h
@@ -6,8 +6,8 @@ protected:
     int hp, dmg;
     string name;
     short *available_moves, moves_count, max_moves;
-public:
     Entity(int _hp, int _dmg, string _name, int _moves_count):hp(_hp), dmg(_dmg), name(_name), moves_count(0), max_moves(_moves_count), available_moves(new short[_moves_count]){}
+public:
     bool Alive();
     void LearnMove(short);
     string Name();
